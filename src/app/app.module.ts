eef430e10bb65fa6e67925614ea9a0e32a0ae7bd
Jerry Olewniczak <jolewniczak@OnBase.net>
2018-03-12 08:40:49 -0400

    basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
+import { MaterialModule } from './material.module';
+import { SharedModule } from './shared.module';
+import { SplitPaneModule } from 'ng2-split-pane/lib/ng2-split-pane';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    SharedModule,
+    SplitPaneModule
   ],
   providers: [],
   bootstrap: [AppComponent]
