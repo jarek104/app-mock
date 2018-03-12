7d11ea90b35c5f2685767444a6eddaad31766f04
Jerry Olewniczak <jolewniczak@OnBase.net>
2018-03-12 10:26:34 -0400

    still doing show/hide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { ObservableMedia } from '@angular/flex-layout';
 import { map } from 'rxjs/operators/map';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +11,23 @@ import { map } from 'rxjs/operators/map';
 })
 export class AppComponent implements OnInit {
 
-  showDocumentTab$?: Observable<'true' | 'false'>;
+  showDocumentTab$?: Observable<boolean>;
+  test: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
   constructor(
     private observableMedia: ObservableMedia,
   ) {}
 
   ngOnInit() {
-
-    this.showDocumentTab$ = this.observableMedia.asObservable().pipe(
-      map(mediaChange => {
-        return mediaChange.mqAlias === 'xs' ? 'true' : 'false';
-      }),
-    );
+    // this.test.next('true');
+    this.showDocumentTab$ = this.test.asObservable();
+    // this.showDocumentTab$ = this.observableMedia.asObservable().pipe(
+    //   map(mediaChange => {
+    //     return mediaChange.mqAlias === 'xs' ? 'true' : 'false';
+    //   }),
+    // );
+  }
+  onClickMe(val: boolean) {
+    this.test.next(false);
   }
 }
 
